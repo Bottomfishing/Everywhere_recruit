f867dcc4d02b975fa269e4cd51b7a8ea9fe4e008
王俊博 <wb1986265022@163.com>
2024-12-26 23:06:45 +0800

    更改home页面导航栏显示样式
    导航栏隐藏功能，但是存在bug

--- 哪都招/js/home.js
+++ 哪都招/js/home.js
@@ -54,3 +54,27 @@ document.getElementById('sidebar1').addEventListener('click', function() {
 	sidebar.classList.toggle('open');
 });
 
+//当页面滚动到轮播图片下面时候，导航栏收起不显示
+ const navbar = document.getElementById('navbar');
+        const slider = document.getElementById('slider');
+        let isAnimating = false;
+
+        window.addEventListener('scroll', () => {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const sliderTop = slider.offsetTop;
+            const sliderHeight = slider.offsetHeight;
+
+            if (scrollTop >= sliderTop + sliderHeight &&!isAnimating) {
+                isAnimating = true;
+                navbar.classList.add('hidden');
+                setTimeout(() => {
+                    isAnimating = false;
+                }, 300);
+            } else if (scrollTop < sliderTop + sliderHeight && navbar.classList.contains('hidden')) {
+                isAnimating = true;
+                navbar.classList.remove('hidden');
+                setTimeout(() => {
+                    isAnimating = false;
+                }, 300);
+            }
+        });
